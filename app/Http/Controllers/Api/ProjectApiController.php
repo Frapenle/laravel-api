d8fd6070fcdb8e69447fc3898b7a49636f218da4
francescotrudu <francescotrudu84@gmail.com>
2023-03-02 17:37:50 +0100

    Added show and axios

--- app/Http/Controllers/Api/ProjectApiController.php
+++ app/Http/Controllers/Api/ProjectApiController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::paginate(5);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show(Project $project)
+    {
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
 }
